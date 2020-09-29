2564fc2210436f68efc46cd2d902565e9cdc0d3c
Joshua Gato <joshuagato37@gmail.com>
2020-09-29 00:34:08 +0000

    update

--- django_vue/settings.py
+++ django_vue/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,6 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     )
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
